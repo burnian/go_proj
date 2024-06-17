dba13411a37af2d02f27f40fab20c6c523f25ee7
burnian <burnian@qq.com>
2024-06-17 18:34:34 +0800

    modify common utils
    Signed-off-by: burnian <burnian@qq.com>

--- common/utils.go
+++ common/utils.go
@@ -1,13 +1,8 @@
 package utils
 
 import (
-	"fmt"
 	"strings"
 )
-
-func init() {
-	fmt.Println("utils.go init()")
-}
 
 func StrSplit(str string, sep string) []string {
 	return strings.Split(str, sep)
--- common/utils_file.go
+++ common/utils_file.go
@@ -2,10 +2,6 @@ package utils
 
 import "fmt"
 
-func init() {
-	fmt.Println("utils_file.go init()")
-}
-
 func Show() {
 	fmt.Println("utils_file.go Show()")
 }
